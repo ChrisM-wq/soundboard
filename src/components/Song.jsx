9f0617c80ce54ae24e19452e4ba9878fc9453a13
Chris McHardy <chrismchardy1@gmail.com>
2023-07-18 22:47:30 +0200

    Styling

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -6,15 +6,18 @@ const Song = () => {
     const iconSize = '2em';
 
     return (
-        <>
-        <div className='flex gap-2'>
-            <AiFillStar size={iconSize}/>
-            <AiOutlineStar size={iconSize}/>
-            <BiSolidLockAlt size={iconSize}/>
-            <AiFillStar size={iconSize}/>
-            <BiSolidLockAlt size={iconSize}/>
+        <div className='flex gap-2 w-full h-'>
+            <div className='flex w-1/2 items-center justify-center'>
+                <iframe src="https://open.spotify.com/embed/track/7HfzbYD6EYmDEjI6CpiWnO?utm_source=generator&theme=0" width="600" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <div className='flex gap-2 w-1/2'>
+                <AiFillStar size={iconSize}/>
+                <AiOutlineStar size={iconSize}/>
+                <BiSolidLockAlt size={iconSize}/>
+                <AiFillStar size={iconSize}/>
+                <BiSolidLockAlt size={iconSize}/>
+            </div>
         </div>
-        </>
     )
 };
 
